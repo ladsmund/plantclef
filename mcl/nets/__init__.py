15348efa3e46907095c5b836d4ae3441e5a4d2fc
mads <ladsmund@gmail.com>
2016-06-13 17:01:35 +0200

    Implement solver and caffe net in the module called mcl

---
+++ mcl/nets/__init__.py
@@ -0,0 +1,5 @@
+from caffe_net import caffenet
+from species_net import speciesnet
+
+reload(caffe_net)
+reload(species_net)
---
+++ mcl/nets/species_net.py
@@ -0,0 +1,30 @@
+import os
+from caffe import layers as L
+from caffe_net import caffenet
+
+DEFAULT_DATA_PATH = os.path.join(os.getcwd(), 'data')
+
+
+def speciesnet(train=True, learn_all=False, source=None, num_classes=None, **kwargs):
+    if source is None:
+        file_name = 'train.txt' if train else 'test.txt'
+        source = os.path.join(DEFAULT_DATA_PATH, file_name)
+
+    if num_classes is None:
+        with open(os.path.join(DEFAULT_DATA_PATH, 'species.txt')) as f:
+            species = f.readlines()
+            num_classes = len(species)
+
+    transform_param = dict(mirror=train, crop_size=227)
+    style_data, style_label = L.ImageData(transform_param=transform_param,
+                                          source=source,
+                                          batch_size=50,
+                                          new_height=256,
+                                          new_width=256,
+                                          ntop=2)
+
+    return caffenet(data=style_data, label=style_label, train=train,
+                    num_classes=num_classes,
+                    classifier_name='fc8_plant',
+                    learn_all=learn_all,
+                    **kwargs)
