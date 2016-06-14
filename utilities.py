da02e497ea7dccc7a5c187a31af8e616b8a1c4c3
mads <ladsmund@gmail.com>
2016-06-14 17:51:45 +0200

    Dsp predictions

--- utilities.py
+++ utilities.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import tempfile
+import os
 
 
 # Helper function for deprocessing preprocessed images, e.g., for display.
@@ -48,3 +50,40 @@ def vis_square(data):
 
     plt.imshow(data);
     plt.axis('off')
+
+
+def disp_predictions(net, image, labels, k=5, name='ImageNet'):
+    input_blob = net.blobs['data']
+    net.blobs['data'].data[0, ...] = image
+    probs = net.forward(start='conv1')['probs'][0]
+    top_k = (-probs).argsort()[:k]
+    print 'top %d predicted %s labels =' % (k, name)
+    print '\n'.join('\t(%d) %5.2f%% %s' % (i+1, 100*probs[p], labels[p])
+                    for i, p in enumerate(top_k))
+
+
+def run_solvers(niter, solvers, disp_interval=10):
+    """Run solvers for niter iterations,
+       returning the loss and accuracy recorded each iteration.
+       `solvers` is a list of (name, solver) tuples."""
+    blobs = ('loss', 'acc')
+    loss, acc = ({name: np.zeros(niter) for name, _ in solvers}
+                 for _ in blobs)
+    for it in range(niter):
+        for name, s in solvers:
+            s.step(1)  # run a single SGD step in Caffe
+            loss[name][it], acc[name][it] = (s.net.blobs[b].data.copy()
+                                             for b in blobs)
+        if it % disp_interval == 0 or it + 1 == niter:
+            loss_disp = '; '.join('%s: loss=%.3f, acc=%2d%%' %
+                                  (n, loss[n][it], np.round(100*acc[n][it]))
+                                  for n, _ in solvers)
+            print '%3d) %s' % (it, loss_disp)
+    # Save the learned weights from both nets.
+    weight_dir = tempfile.mkdtemp()
+    weights = {}
+    for name, s in solvers:
+        filename = 'weights.%s.caffemodel' % name
+        weights[name] = os.path.join(weight_dir, filename)
+        s.net.save(weights[name])
+    return loss, acc, weights
