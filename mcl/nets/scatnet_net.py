2f9c25c140a4e71c6d91232a8477545109ea813d
mads <ladsmund@gmail.com>
2016-06-17 16:14:57 +0200

    scatnet_net shouis verified to produce a convolutional network equivalent to my scatnet implementation

--- mcl/nets/scatnet_net.py
+++ mcl/nets/scatnet_net.py
@@ -9,14 +9,16 @@ EltwiseParameter_EltwiseOp_SUM = 1
 EltwiseParameter_EltwiseOp_MAX = 2
 
 
-def conv_layer(bottom, dim, kernel_size, name, group=1, stride=1):
+def conv_layer(bottom, dim, kernel_size, name, group=1, stride=1, pad=None):
+    if pad is None:
+        pad = kernel_size // 2
     return L.Convolution(
         bottom,
         name=name,
         group=group,
         convolution_param=dict(
             num_output=dim, kernel_size=kernel_size,
-            stride=stride, pad=kernel_size // 2,
+            stride=stride, pad=pad,
             weight_filler=dict(type="constant", value=0),
             bias_filler=dict(type="constant", value=0)))
 
@@ -37,29 +39,39 @@ def scat_layer(bottom, dim, kernel_size, name, group=1):
     return res_add
 
 
-def gen_prototxt(nangles=1, max_order=1, scales=[1], data=L.Input(shape=dict(dim=[1, 1, 100, 100])),
+def gen_prototxt(nangles, max_order, scales, kernel_size, data=L.Input(shape=dict(dim=[1, 1, 256, 256])),
                  output_path=None):
     n = caffe.NetSpec()
     n.data = data
 
     nscales = len(scales)
 
+    global scat_count
+    scat_count = -1
+
     def new_scat_layer(input, id, dim_out, dim_in):
-        return scat_layer(input, dim=dim_out, group=dim_in, kernel_size=15, name='scat%i' % id)
+        global scat_count
+        scat_count += 1
+        return scat_layer(input, dim=dim_out, group=dim_in, kernel_size=kernel_size,
+                          name='scat%i_%i_%ito%i' % (id, scat_count, dim_in, dim_out))
 
     dim_total = 1
-    layers = [[(data, -1)]]
+    layers = [[(data, [None])]]
     for o in range(max_order):
         layer = []
         for s in range(nscales):
             for c0, s0 in layers[-1]:
-                if s <= s0:
+                if s0[-1] is not None and s <= s0[-1]:
                     continue
+
                 dim_in = nangles ** o
                 dim_out = nangles ** (o + 1)
                 dim_total += dim_out
                 c = new_scat_layer(c0, s, dim_out, dim_in)
-                layer.append((c, s))
+                layer.append((c, s0 + [s]))
+
+                print "%s (%i)" % ("->".join(map(str, (s0 + [s]))), dim_out)
+
         layers.append(layer)
 
     layers = zip(*reduce(operator.add, layers))[0]
@@ -67,35 +79,18 @@ def gen_prototxt(nangles=1, max_order=1, scales=[1], data=L.Input(shape=dict(dim
 
     print "Total output dimensionality: %i" % dim_total
 
+    stride = 2 ** (max(scales))
+    n.output = conv_layer(concat,
+                          dim=dim_total,
+                          group=dim_total,
+                          kernel_size=kernel_size,
+                          name='psi',
+                          stride=stride)
 
+    # slices = L.Slice(n.output, slice_point=[10,20,30], ntop=4)
+    # for i in range(4):
+    #     setattr(n, "test_%i"%i, slices[i])
 
-    # o1s1 = scat_layer(n.data, dim=6, kernel_size=15, name='scat0')
-    # o1s2 = scat_layer(n.data, dim=6, kernel_size=15, name='scat1')
-    # o1s3 = scat_layer(n.data, dim=6, kernel_size=15, name='scat2')
-    #
-    # o2s2 = scat_layer(o1s1, dim=36, kernel_size=15, name='scat1', group=6)
-    # o2s3 = scat_layer(o1s2, dim=36, kernel_size=15, name='scat2', group=6)
-    #
-    # o3s3 = scat_layer(o2s2, dim=216, kernel_size=15, name='scat2', group=36)
-
-    # n.ouet = o3s3
-
-    # concat = L.Concat(n.data, o1s1, o1s2, o1s3, o2s2, o2s3, o3s3)
-
-    # n.output = concat
-
-    stride = 2**(max(scales)-1)
-    print "stride: %i" % stride
-    n.output = conv_layer(concat, dim=dim_total, group=dim_total, kernel_size=15, name='psi', stride=stride)
-    # n.output = conv_layer(concat, dim=306, group=1, kernel_size=15, name='psi', stride=8)
-    # n.output = L.Pooling(concat, kernel_size=7, stride=4, pool=P.Pooling.MAX)
-    # n.output = conv_layer(concat, dim=1, kernel_size=7, name='psi', stride=8)
-
-    # proto_str = str(n.to_proto())
-    # proto_path = "./test.prototxt"
-    # with open(proto_path, 'w+') as f:
-    #     f.write(proto_str)
-    #
     proto_str = str(n.to_proto())
 
     if output_path:
